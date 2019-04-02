b833566916874747b95ace1b2625c0e0e620b533
SoumiDas <33958408+SoumiDas@users.noreply.github.com>
2019-04-02 14:41:19 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+
+#Import other modules here
+
+class CNN(object):
+
+	def __init__(self):
+		pass
---
+++ train.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+import dataload
+import model
+
+#Load mini batch data
+#Call the CNN model and train under train()
+#Print results for the test set under the best hyperparamter setting under test()
+
+def train():
+	pass
+
+def test():
+	pass
